3997decf49750dba27d8e28be306449d4cf22b5b
Juan Taborda <juanp.gomezt@upb.edu.co>
2025-10-28 15:26:03 -0500

    RETO UNIDAD 5

--- .github/Gráficas/graficas.py
+++ .github/Gráficas/graficas.py
@@ -1,17 +1,8 @@
 import matplotlib.pyplot as plt
-import numpy as np
 
-# Datos
-x = np.linspace(0, 10, 100)
-y = np.sin(x)
+x = [1, 2, 3]
+y = [10, 20, 30]
 
-# Crear la gráfica
 plt.plot(x, y)
-
-# Agregar título y etiquetas
-plt.title('Gráfica de Seno')
-plt.xlabel('X')
-plt.ylabel('sin(X)')
-
-# Mostrar la gráfica
+plt.title("Prueba Matplotlib")
 plt.show()
